93a5e2696beecae5b13f85e0534585246314c8f3
Stefan `Sec` Zehl <sec@42.org>
2011-06-13 23:19:18 +0200

    Enable systick (10ms) by default and allow modules to be called from systick
    Also add VoltageCheck once per sec by default

--- modules/default.c
+++ modules/default.c
@@ -3,5 +3,18 @@
 /**************************************************************************/
 
 void module_default(void) {
+    systickInit(10);
     return;
-}
+};
+
+// every 10 ms
+void tick_default(void) {
+    static int ctr;
+    ctr++;
+    if(ctr>100){
+        VoltageCheck();
+        ctr=0;
+    };
+
+    return;
+};
