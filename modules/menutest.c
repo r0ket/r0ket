6d4c09066d6a2580756b2b58d7905867f4fafd3b
Stefan `Sec` Zehl <sec@42.org>
2011-06-17 16:19:33 +0200

    Just fix some compile errors

--- modules/menutest.c
+++ modules/menutest.c
@@ -64,43 +64,6 @@ static menuentry menu[] = {
 static const struct MENU mainmenu = {"Mainmenu", menu};
 
 void handleMenu(const struct MENU *the_menu) ;
-void delayms(int);
-
-uint8_t getInput(void) {
-    uint8_t result = BTN_NONE;
-
-    if (gpioGetValue(RB_BTN3)==0) {
-        while(gpioGetValue(RB_BTN3)==0);
-        result += BTN_UP;
-    }
-
-    if (gpioGetValue(RB_BTN2)==0) {
-        while(gpioGetValue(RB_BTN2)==0);
-        result += BTN_DOWN;
-    }
-
-    if (gpioGetValue(RB_BTN4)==0) {
-        while(gpioGetValue(RB_BTN4)==0);
-        result += BTN_ENTER;
-    }
-
-    if (gpioGetValue(RB_BTN0)==0) {
-        while(gpioGetValue(RB_BTN0)==0);
-        result += BTN_LEFT;
-    }
-
-    if (gpioGetValue(RB_BTN1)==0) {
-        while(gpioGetValue(RB_BTN1)==0);
-        result += BTN_RIGHT;
-    }
-
-    if (result == (BTN_LEFT+BTN_TOP+BTN_ENTER)){ /* Development hack */
-        gotoISP();
-    }
-
-    return result;
-}
-
 
 void module_menutest(void) {
 
