1848185e801f03a72040ae0c22a769dd0b12ff73
schneider <schneider@blinkenlichts.net>
2011-07-10 15:41:16 +0200

    more minimalistic minimal.c

--- firmware/applications/minimal.c
+++ firmware/applications/minimal.c
@@ -10,16 +10,10 @@ void backlightInit(void);
 /**************************************************************************/
 
 void main_minimal(void) {
-    //int yctr=8;
     int dx=0;
     char key;
-    int ctr = 0;
     backlightInit();
     font_direction = FONT_DIR_LTR; // LeftToRight is the default
-    //yctr=18;
-    int toggle = 0;
-    volatile int foo;
-    volatile char foo2[] = "12345";
     while (1) {
 
         lcdDisplay(0);
@@ -33,17 +27,10 @@ void main_minimal(void) {
             DoString(0,8,"Enter ISP!");
             lcdDisplay(0);
             ISPandReset(5);
-        }else if(key==BTN_RIGHT){
-            gpioSetValue (RB_LED1, toggle);
-            foo = strspn(foo2, "123456789");
-            gpioSetValue (RB_LED2, toggle);
-            toggle = 1 - toggle;
         }
 
-        // Display nickname
         //font = &Font_Ubuntu36pt;
-        dx=DoString(0,0,"Test");
-        dx=DoInt(dx,0,ctr++);
+        dx=DoString(0,0,"Hello World");
     }
     return;
 }
