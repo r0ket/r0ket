d25d57313c35c72c346190be39c90b2db444b6ed
Stefan `Sec` Zehl <sec@42.org>
2011-07-13 00:23:58 +0200

    Update exe to use new f_get_rc_string()

--- firmware/applications/exe.c
+++ firmware/applications/exe.c
@@ -12,17 +12,7 @@
 /**************************************************************************/
 
 void put_rc_y (FRESULT rc, int y) {
-	const TCHAR *p =
-		_T("OK\0DISK_ERR\0INT_ERR\0NOT_READY\0NO_FILE\0NO_PATH\0INVALID_NAME\0")
-		_T("DENIED\0EXIST\0INVALID_OBJECT\0WRITE_PROTECTED\0INVALID_DRIVE\0")
-		_T("NOT_ENABLED\0NO_FILE_SYSTEM\0MKFS_ABORTED\0TIMEOUT\0LOCKED\0")
-		_T("NOT_ENOUGH_CORE\0TOO_MANY_OPEN_FILES\0");
-	FRESULT i;
-
-	for (i = 0; i != rc && *p; i++) {
-		while(*p++) ;
-	}
-    DoString(0,y,p);
+    DoString(0,y,f_get_rc_string(rc));
 }
 
 void put_rc (FRESULT rc){
