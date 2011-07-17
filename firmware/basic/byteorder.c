bfbc6c2732c310c0f0827c5a16116c406495f13e
schneider <schneider@blinkenlichts.net>
2011-07-17 20:32:17 +0200

    openbeacon: save seq to file

--- firmware/basic/byteorder.c
+++ firmware/basic/byteorder.c
@@ -7,3 +7,13 @@ void uint32touint8p(uint32_t v, uint8_t *p)
     *p++ = (v>> 8)&0xFF;
     *p++ = (v>> 0)&0xFF;
 }
+
+uint32_t uint8ptouint32(uint8_t *p)
+{
+    uint32_t v;
+    v |= *p++; v<<=8;
+    v |= *p++; v<<=8;
+    v |= *p++; v<<=8;
+    v |= *p;
+    return v;
+}
--- firmware/basic/byteorder.h
+++ firmware/basic/byteorder.h
@@ -1,5 +1,9 @@
 #ifndef _BYTEORDER_H_
 #define _BYTEORDER_H_
+
+#include <stdint.h>
+
 void uint32touint8p(uint32_t v, uint8_t *p);
+uint32_t uint8ptouint32(uint8_t *p);
 
 #endif
