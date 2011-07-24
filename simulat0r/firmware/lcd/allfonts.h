009e235af095171c790a5e17ea08049332bdf0dc
roy rocket <royrocket@custom.ray.net>
2011-07-24 23:32:08 +0200

    oops, added real generated files, undone...

--- simulat0r/firmware/lcd/allfonts.h
+++
@@ -1,7 +0,0 @@
-#include <lcd/fonts.h>
-#include <lcd/fonts/invaders.h>
-#include <lcd/fonts/orbitron14.h>
-#include <lcd/fonts/smallfonts.h>
-#include <lcd/fonts/ubuntu18.h>
-#include <lcd/fonts/ubuntu29.h>
-#include <lcd/fonts/ubuntu36.h>
