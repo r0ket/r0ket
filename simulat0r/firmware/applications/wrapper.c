603ba7049c9b920a9cfe49960d3e6e9212192dd0
roy rocket <royrocket@custom.ray.net>
2011-07-24 23:23:02 +0200

    New wrapper includes

---
+++ simulat0r/firmware/applications/wrapper.c
@@ -0,0 +1,11 @@
+void main_default(void);
+void tick_default(void);
+void main_life(void);
+void tick_life(void);
+
+void wrapper(void){
+main_default();
+main_life();
+}
+void tick_wrapper(void){
+}
---
+++ simulat0r/firmware/lcd/allfonts.h
@@ -0,0 +1,7 @@
+#include <lcd/fonts.h>
+#include <lcd/fonts/invaders.h>
+#include <lcd/fonts/orbitron14.h>
+#include <lcd/fonts/smallfonts.h>
+#include <lcd/fonts/ubuntu18.h>
+#include <lcd/fonts/ubuntu29.h>
+#include <lcd/fonts/ubuntu36.h>
