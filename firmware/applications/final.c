c856eb4f99dc6da568f54044e7404bb785ec38eb
Stefan `Sec` Zehl <sec@42.org>
2011-08-03 18:52:56 +0200

    Remove stuff for old loadable requirements

--- firmware/applications/final.c
+++ firmware/applications/final.c
@@ -15,25 +15,9 @@
 void init_nick();
 void fancyNickname();
 
-#include "lcd/allfonts.h"
-void forLoadables(int i){
-    if(i){
-        lcdSetPixel(0,0);
-        font=&Font_Invaders;
-        ECIES_encyptkeygen(0,0,0,0,0,0);
-        systickGetTicks();
-        ECIES_embedded_public_key_validation();
-        point_copy(0,0,0,0);
-        point_is_zero(0,0);
-        bitstr_parse_export(0,0);
-        nrf_rcv_pkt_time_encr();
-    };
-};
-
 void main_final(void) {
     //checkFirstBoot();
     init_final();
-    forLoadables(0);
     menuflags|=MENU_TIMEOUT;
 
     while(1){
