177acbeffa07443a0e5f3f99bb0750467564bf23
Stefan `Sec` Zehl <sec@42.org>
2011-08-12 20:27:36 +0200

    Restore state of LED at end

--- firmware/l0dable/blink.c
+++ firmware/l0dable/blink.c
@@ -5,9 +5,11 @@
 #include "usetable.h"
 
 void ram(void){
+    char x = gpioGetValue(RB_LED1);
+
 	for (int x=0;x<20;x++){
 		gpioSetValue (RB_LED1, x%2); 
 		delayms(50);
 	};
-    gpioSetValue (RB_LED1, 0); 
+    gpioSetValue (RB_LED1, x); 
 };
