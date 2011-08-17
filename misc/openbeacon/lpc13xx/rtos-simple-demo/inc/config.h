e6fa3b857347b74251b428161f17c8353f3d080e
schneider <schneider@blinkenlichts.net>
2011-08-17 22:40:19 +0200

    removed openbeacon files

--- misc/openbeacon/lpc13xx/rtos-simple-demo/inc/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CONFIG_H__
-#define __CONFIG_H__
-
-#define ENABLE_FREERTOS
-
-#define LED_PORT 1		// Port for led
-#define LED_BIT 9		// Bit on port for led
-
-#endif/*__CONFIG_H__*/
--- misc/openbeacon/lpc13xx/sound/inc/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CONFIG_H__
-#define __CONFIG_H__
-
-#define BUT1_PORT 2	// Port for Button 1
-#define BUT1_PIN 9	// Bit on port forButton 1
-#define BUT2_PORT 1	// Port for Button 2
-#define BUT2_PIN 4	// Bit on port forButton 2
-
-#endif/*__CONFIG_H__*/
